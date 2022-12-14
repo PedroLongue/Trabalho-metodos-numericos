7b93e29b9e915e2c948aece8e30d9265589e6fc7
Pedro Longue <pedrolongue13@gmail.com>
2022-12-14 17:03:02 -0300

    termina a primeira parte do trab1

--- rk3.py
+++ rk3.py
@@ -1,14 +1,15 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 
-def rungeKutta3System(equations, t, tMax, constant, deltaT):
+from random import random
+
+def rungeKuttaSystem(equations, t, tMax, constant, deltaT):
     increment = np.arange(t, tMax, deltaT) #incrementa do t inical ate o t final usando o delta t
 
     c_a=[len(increment)]
     c_b=[len(increment)]
     c_c=[len(increment)]
 
-    #condições iniciais
     c_a[0]= equations[0]
     c_b[0]= equations[1]
     c_c[0]= equations[2]
@@ -21,11 +22,6 @@ def rungeKutta3System(equations, t, tMax, constant, deltaT):
 
     def derivateC_C(t,c_a,c_b,c_c):
         return ((-constant[2]*c_a*c_c+c_b)-2*c_c)
-
-    for i in range(len(equations)):
-        print(f'f{i+1}({t}) = {equations[i]},  {t=},  {deltaT=}')
-
-    print('\n')
 
     for j in range(t, len(increment)-1):
         k1Ca = derivateC_A(increment[j], c_a[j], c_b[j], c_c[j])
@@ -61,28 +57,86 @@ def rungeKutta3System(equations, t, tMax, constant, deltaT):
  
 def renderGraph(equations, t, tMax, constant, deltaT):
     # print("aaa", len(resultRk[3]))
-    resultRk = rungeKutta3System(equations,t,tMax,constant,deltaT)
-    plt.plot(resultRk[3],resultRk[0],color='purple', label='ca')
-    plt.plot(resultRk[3],resultRk[1],color='black', label='cb')
-    plt.plot(resultRk[3],resultRk[2],color='blue', label='cc')
-    plt.legend(title="C",loc=0)
-    plt.xlabel("t(s)")
-    plt.ylabel("C")
-    plt.show()
-    print("a=", resultRk[0][len(resultRk[0])-1])
-    print("b=", resultRk[1][len(resultRk[1])-1])
-    print("c=", resultRk[2][len(resultRk[2])-1])
+    hex = '#%06X' % round(random() * 0xffffff)
+    hex1 = '#%06X' % round(random() * 0xffffff)
+    hex2 = '#%06X' % round(random() * 0xffffff)
+    resultRk = rungeKuttaSystem(equations,t,tMax,constant,deltaT)
+
+    plot.plot(resultRk[3],resultRk[0],color=hex, label='ca')
+    plot.plot(resultRk[3],resultRk[1],color=hex1, label='cb')
+    plot.plot(resultRk[3],resultRk[2],color=hex2, label='cc')
+    plot.legend(title="C")
+    plot.xlabel("t(s)")
+    plot.ylabel("C")
+    plot.show()
+
+    print("O aproximado de Ca=", resultRk[0][len(resultRk[0])-1])
+    print("O aproximado de Cb=", resultRk[1][len(resultRk[1])-1])
+    print("O aproximado de Cc=", resultRk[2][len(resultRk[2])-1])
+
+def refinamento(equations, t, tMax, constant, deltaT0, deltaT1, deltaT):
+    DeltaT = np.arange(deltaT0, deltaT1, 0.1)
+    x, (ca, cb, cc)= plot.subplots(1,3)
+    for i in range(0,len(DeltaT)):
+        hex = '#%06X' % round(random() * 0xffffff)
+        rungeKuttaSystem1 = rungeKuttaSystem(equations, t, tMax, constant, DeltaT[i])
+        print(rungeKuttaSystem1[0],rungeKuttaSystem1[3])
+        ca.plot(rungeKuttaSystem1[3],rungeKuttaSystem1[0],color=hex, label='%.2f'%(float(DeltaT[i])))
+        ca.legend(title="delta T")
+        ca.set_xlabel("t(s)")
+        ca.set_ylabel("ca")
+        cb.plot(rungeKuttaSystem1[3],rungeKuttaSystem1[1],color=hex, label='%.2f'%(float(DeltaT[i])))
+        cb.legend(title="delta T")
+        cb.set_xlabel("t(s)")
+        cb.set_ylabel("cb")
+        cc.plot(rungeKuttaSystem1[3],rungeKuttaSystem1[2],color=hex, label='%.2f'%(float(DeltaT[i])))
+        cc.legend(title="delta T")
+        cc.set_xlabel("t(s)")
+        cc.set_ylabel("cc") 
+    plot.show()  
+
+def sensibilidade(equations, t, tMax, constant, deltaT):
+    sensitivityAlpha = [constant[0]-0.1*constant[0],constant[0]-0.05*constant[0], constant[0], constant[0]+0.05*constant[0], constant[0]+0.10*constant[0]]
+    sensitivityBeta = [constant[1]-0.1*constant[1],constant[1]-0.05*constant[1], constant[1], constant[1]+0.05*constant[1], constant[1]+0.10*constant[1]]
+    sensitivityGamma = [constant[2]-0.1*constant[2],constant[2]-0.05*constant[2], constant[2], constant[2]+0.05*constant[2], constant[2]+0.10*constant[2]]
+    x, (alphaGraph, betaGraph, gammaGraph)= plot.subplots(1,3)
+
+    for i in range(0, len(sensitivityAlpha)):
+        hex = '#%06X' % round(random() * 0xffffff)
+        rungeKuttaAlpha = rungeKuttaSystem(equations, t, tMax, constant, deltaT)
+        rungeKuttaBeta = rungeKuttaSystem(equations, t, tMax, constant, deltaT)
+        rungeKuttaGamma = rungeKuttaSystem(equations, t, tMax, constant, deltaT)
+
+        alphaGraph.plot(rungeKuttaAlpha[3],rungeKuttaAlpha[0],color=hex, label='%.2f'%(float(sensitivityAlpha[i])))
+        alphaGraph.legend(title="alfa")
+        alphaGraph.set_xlabel("t(s)")
+        alphaGraph.set_ylabel("ca")
+        betaGraph.plot(rungeKuttaBeta[3],rungeKuttaBeta[1],color=hex, label='%.2f'%(float(sensitivityBeta[i])))
+        betaGraph.legend(title="beta")
+        betaGraph.set_xlabel("t(s)")
+        betaGraph.set_ylabel("cb")
+        gammaGraph.plot(rungeKuttaGamma[3],rungeKuttaGamma[2],color=hex, label='%.2f'%(float(sensitivityGamma[i])))
+        gammaGraph.legend(title="gamma")
+        gammaGraph.set_xlabel("t(s)")
+        gammaGraph.set_ylabel("cc") 
+    plot.show()
 
 ca, cb, cc = 1, 0, 1
 t0 = 0
 tMax = 1.5
 deltaT = 0.5
-alfa, beta, gama = 1, 1, 1
+alfa, beta, gamma = 1, 1, 1
 
-# rungeKutta3System([ca, cb, cc],t0,tMax,[alfa, beta, gama],deltaT)
+#--------------REFINAMENTO--------------#
+deltaT0 = 0.1
+deltaT1 = 0.5
 
-# print("Ca: ", rungeKutta3System([ca, cb, cc],t0,tMax,[alfa, beta, gama],deltaT)[0])
-# print("Cb: ", rungeKutta3System([ca, cb, cc],t0,tMax,[alfa, beta, gama],deltaT)[1])
-# print("Cc: ", rungeKutta3System([ca, cb, cc],t0,tMax,[alfa, beta, gama],deltaT)[2])
+# rungeKuttaSystem([ca, cb, cc],t0,tMax,[alfa, beta, gamma],deltaT)
 
-renderGraph([ca, cb, cc],t0,tMax,[alfa, beta, gama],deltaT)
+# print("Ca: ", rungeKuttaSystem([ca, cb, cc],t0,tMax,[alfa, beta, gamma],deltaT)[0])
+# print("Cb: ", rungeKuttaSystem([ca, cb, cc],t0,tMax,[alfa, beta, gamma],deltaT)[1])
+# print("Cc: ", rungeKuttaSystem([ca, cb, cc],t0,tMax,[alfa, beta, gamma],deltaT)[2])
+
+renderGraph([ca, cb, cc],t0,tMax,[alfa, beta, gamma],deltaT)
+refinamento([ca,cb,cc],t0, tMax, [alfa,beta, gamma],deltaT0, deltaT1,deltaT)
+sensibilidade([ca,cb,cc], t0, tMax, [alfa,beta, gamma], deltaT)
